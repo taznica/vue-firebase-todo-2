f85a88fc9880b2ef9ab4cb0890148a0a831c3e5a
ttsutchi <tailegog@gmail.com>
2019-02-12 19:54:59 +0900

    [Add] add basic database features with Firebase

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <button type="button" class="btn btn-default" @click="login">
+        匿名ユーザーでログイン
+      </button>
+      <button type="button" class="btn btn-default" @click="pushData">
+        送信
+      </button>
+    </div>
+    <div>
+      <ul v-for="item in list" :key="item.name">
+        <li>{{item.name}} / {{item.message}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import firebase from 'firebase'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: {},
+
+
+  created() {
+    firebase.auth().onAuthStateChanged( user => {
+      if (user) {
+        // User is signed in.
+        // eslint-disable-next-line
+        console.log('is login.')
+      } else {
+        // No user is signed in.
+        // eslint-disable-next-line
+        console.log('No user is signed in.')
+      }
+    })
+    this.listen()
+  },
+
+  methods: {
+    // ログイン関数
+    login () {
+      firebase.auth().signInAnonymously().then(e => {
+        // eslint-disable-next-line
+        console.log(e)
+        this.listen()
+      }).catch((error) => {
+        // ログインのエラーメッセージ
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // eslint-disable-next-line
+        console.log('ログインエラーメッセージ', errorCode, errorMessage)
+      });
+    },
+    // データベースの変更を購読、最新状態をlistにコピーする
+    listen () {
+      firebase.database().ref('myBoard/').on('value', snapshot => {
+        if (snapshot) {
+          const rootList = snapshot.val()
+          let list = []
+          Object.keys(rootList).forEach((val, key) => {
+            rootList[val].id = val
+            list.push(rootList[val])
+          })
+          this.list = list
+        }
+      })
+    },
+    // ダミーデータをfirebaseに送信
+    pushData () {
+      firebase.database().ref('myBoard/').push({
+        name: 'test',
+        message: 'hoge'
+      }).then(this.listen())
+    },
+  },
+
+  data() {
+    return {
+      list: []
+    }
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
 
 new Vue({
   render: h => h(App),
